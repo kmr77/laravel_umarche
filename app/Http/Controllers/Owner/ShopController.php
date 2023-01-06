4974d2240d38f559ea6d7e172dfbedc7b8809657
KIMURA MASAYUKI <>
2023-01-06 14:19:46 +0900

    index画面

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -30,15 +30,15 @@ class ShopController extends Controller
     }
 
     public function index(){
-        $ownerId = Auth::id();
-        $shops = Shop::where('owner_id', $ownerId)->get();
+        // $ownerId = Auth::id();
+        $shops = Shop::where('owner_id', Auth::id())->get();
 
         return view('owner.shops.index', compact('shops'));
     }
 
     public function edit($id)
     {
-        dd(Shop::findOrFail($id));
+        Shop::findOrFail($id);
     }
 
     public function update(Request $request, $id)
