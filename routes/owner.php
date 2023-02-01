334b20000480138fa149efecc7fb4a272f13e834
KIMURA MASAYUKI <>
2023-02-01 12:09:55 +0900

    ログアウト時のリダイレクト

--- routes/owner.php
+++ routes/owner.php
@@ -46,11 +46,11 @@ Route::get('/dashboard', function () {
 })->middleware(['auth:owners'])->name('dashboard');
 
 
-// Route::middleware('guest')->group(function () {
-//     Route::get('register', [RegisteredUserController::class, 'create'])
-//                 ->name('register');
+Route::middleware('guest')->group(function () {
+    // Route::get('register', [RegisteredUserController::class, 'create'])
+    //             ->name('register');
 
-//     Route::post('register', [RegisteredUserController::class, 'store']);
+    // Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
